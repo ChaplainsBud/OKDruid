55111cf576d138c84c616d55477d81caebb0b6bc
Matthew Leroe <matthewleroe@Matthews-MBP.fios-router.home>
2020-06-11 15:34:16 -0400

    First

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+
+    let x = document.querySelectorAll(".mySlides");
+
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    }
+
+    x[slideIndex-1].style.display = "block";
+
+    setTimeout(carousel, 5000); // change every 2 seconds
+}
+
+
+
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
